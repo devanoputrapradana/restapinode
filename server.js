5068892291f4828c969a4a5b6dd7c61e5e138c80
devanoputrapradana <devanoputrapradana99@gmail.com>
2021-08-18 17:12:54 +0700

    Menambahkan file Server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+//parse application/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//panggil routes
+//var routes = require('./routes');
+//routes(app);
+
+app.listen(3000, () => {
+    console.log(`Server started on port`);
+});
